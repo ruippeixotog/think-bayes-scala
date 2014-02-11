6a8df36684ea98721f1c99e4386a49c34f9a60a7
Rui Gonçalves <ruippeixotog@gmail.com>
2014-02-11 21:51:35 +0000

    Add the problem statement of each example.

--- src/main/scala/thinkbayes/examples/Dice.scala
+++ src/main/scala/thinkbayes/examples/Dice.scala
@@ -10,6 +10,16 @@ class Dice(hypos: Seq[Int]) extends Suite[Int, Int] {
     if(hypo < data) 0 else 1.0 / hypo
 }
 
+/**
+ * Application for solving the dice problem (page 19):
+ *
+ * "Suppose I have a box of dice that contains a 4-sided die, a 6-sided die, an 8-sided die, a
+ * 12-sided die, and a 20-sided die. If you have ever played Dungeons & Dragons, you know
+ * what I am talking about.
+ *
+ * Suppose I select a die from the box at random, roll it, and get a 6. What is the probability
+ * that I rolled each die?"
+ */
 object DiceApp extends App {
   val suite = new Dice(List(4, 6, 8, 12, 20))
 
--- src/main/scala/thinkbayes/examples/MM.scala
+++ src/main/scala/thinkbayes/examples/MM.scala
@@ -33,6 +33,21 @@ class MM(hypos: Seq[Char], hypoDefs: Map[Char, Map[Bag, Mix]]) extends Suite[Cha
     hypoDefs(hypo)(data._1).getOrElse(data._2, 0).toDouble
 }
 
+/**
+ * Application for solving the M&M problem (page 6):
+ *
+ * "M&M’s are small candy-coated chocolates that come in a variety of colors. Mars, Inc.,
+ * which makes M&M’s, changes the mixture of colors from time to time.
+ *
+ * In 1995, they introduced blue M&M’s. Before then, the color mix in a bag of plain M&M’s
+ * was 30% Brown, 20% Yellow, 20% Red, 10% Green, 10% Orange, 10% Tan. Afterward
+ * it was 24% Blue , 20% Green, 16% Orange, 14% Yellow, 13% Red, 13% Brown.
+ *
+ * Suppose a friend of mine has two bags of M&M’s, and he tells me that one is from 1994
+ * and one from 1996. He won’t tell me which is which, but he gives me one M&M from
+ * each bag. One is yellow and one is green. What is the probability that the yellow one
+ * came from the 1994 bag?"
+ */
 object MMApp extends App {
   val hypoA = Map("bag1" -> mix94, "bag2" -> mix96)
   val hypoB = Map("bag1" -> mix96, "bag2" -> mix94)
