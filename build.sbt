cddbecea67b61f312a6fdffed48fdeb6b039ac3b
Rui Gonçalves <ruippeixotog@gmail.com>
2014-02-11 23:07:05 +0000

    Add plotting capabilities as an extension to Pmfs.

--- build.sbt
+++ build.sbt
@@ -1,1 +1,3 @@
+libraryDependencies += "com.github.wookietreiber" %% "scala-chart" % "latest.integration"
+
 initialCommands in console := "import thinkbayes._; import thinkbayes.examples._"
---
+++ src/main/scala/thinkbayes/extensions/Plotting.scala
@@ -0,0 +1,30 @@
+package thinkbayes.extensions
+
+import org.jfree.chart.StandardChartTheme
+import scalax.chart._
+import scalax.chart.Charting._
+import thinkbayes._
+import scalax.chart.ChartFactories.BarChart
+
+object Plotting {
+  implicit val chartTheme = StandardChartTheme.createLegacyTheme()
+
+  implicit class PlottablePmf[K](val pmf: Pmf[K]) extends AnyVal {
+
+    def plotBar()(implicit ord: K => Ordered[K]): CategoryChart = {
+      val chart = BarChart(pmf.hist.toSeq.toCategoryDataset)
+      chart.rangeAxisLabel = "probability"
+      chart.show
+      chart
+    }
+
+    def plotXY(xLabel: String = "", seriesName: String = "")(implicit ord: Ordering[K], asNum: K => Number): XYChart = {
+      val chart = XYLineChart(pmf.hist.toSeq.toXYSeriesCollection())
+      chart.domainAxisLabel = xLabel
+      chart.rangeAxisLabel = "probability"
+      chart.labelGenerator = None
+      chart.show
+      chart
+    }
+  }
+}
