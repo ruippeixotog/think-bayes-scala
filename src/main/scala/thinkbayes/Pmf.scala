13412d72e8dd051ffc454718af41cf8aaf3f4e2c
Rui Gonçalves <ruippeixotog@gmail.com>
2014-02-16 23:15:00 +0000

    Add some documentation taken from the original thinkbayes code.

--- src/main/scala/thinkbayes/Pmf.scala
+++ src/main/scala/thinkbayes/Pmf.scala
@@ -3,19 +3,35 @@ package thinkbayes
 class Pmf[K] {
   var hist = Map[K, Double]()
 
+  /**
+   * Gets the probability associated with a given key.
+   * @param key the key whose probability is to be returned
+   * @return the probability associated with the given key.
+   */
   def prob(key: K) = hist.getOrElse(key, 0.0)
 
   def set(key: K, prob: Double) = hist += (key -> prob)
   def incr(key: K) = hist += (key -> (hist.getOrElse(key, 0.0) + 1))
   def mult(key: K, factor: Double) = hist += (key -> (hist.getOrElse(key, 0.0) * factor))
 
+  /**
+   * Returns the value with the highest probability.
+   * @return the value with the highest probability.
+   */
   def max: K = hist.maxBy(_._2)._1
 
+  /**
+   * Normalizes this PMF so the sum of all probabilities is 1.0.
+   */
   def normalize() {
     val sum = hist.values.sum
     hist = hist.mapValues(_ / sum)
   }
 
+  /**
+   * Computes the mean of the PMF.
+   * @return the mean of the PMF.
+   */
   def mean(implicit num: Numeric[K]): Double =
     hist.map { case (h, prob) => num.toDouble(h) * prob }.sum
 
--- src/main/scala/thinkbayes/Suite.scala
+++ src/main/scala/thinkbayes/Suite.scala
@@ -1,14 +1,33 @@
 package thinkbayes
 
 abstract class Suite[H, D] extends Pmf[H] {
+
+  /**
+   * Computes the likelihood of a given data under an hypothesis.
+   * @param data the representation of the data whose likelihood is to be returned
+   * @param hypo the representation of the hypothesis
+   * @return the likelihood of the given data under the given hypothesis.
+   */
   def likelihood(data: D, hypo: H): Double
 
-  def update(data: D) = {
+  /**
+   * Updates each hypothesis based on the given data.
+   * Modifies the suite directly; if you want to keep the original, make a copy.
+   * @param data the representation of the data to use to update the suite
+   */
+  def update(data: D) {
     hist = hist.map { case (h, prob) => (h, prob * likelihood(data, h)) }
     normalize()
   }
 
-  def updateSet(dataset: TraversableOnce[D]) = {
+  /**
+   * Updates each hypothesis based on the dataset.
+   * This is more efficient than calling `update` repeatedly because it waits until the end to
+   * `normalize`.
+   * Modifies the suite directly; if you want to keep the original, make a copy.
+   * @param dataset a sequence of data values to use to update the suite
+   */
+  def updateSet(dataset: TraversableOnce[D]) {
     dataset.foreach { data =>
       hist = hist.map { case (h, prob) => (h, prob * likelihood(data, h)) }
     }
