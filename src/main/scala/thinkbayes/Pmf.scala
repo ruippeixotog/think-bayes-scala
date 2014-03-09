8e32114d47b545490bf5277f79bfed8cfc9339ff
Rui Gonçalves <ruippeixotog@gmail.com>
2014-03-09 22:25:05 +0000

    Further improve Scala collection integration.

--- src/main/scala/thinkbayes/Pmf.scala
+++ src/main/scala/thinkbayes/Pmf.scala
@@ -15,6 +15,7 @@ case class Pmf[K](hist: Map[K, Double]) extends Map[K, Double] with MapLike[K, D
   // overloaded methods to deal with the fact that a Pmf has a fixed value type
   def +(kv: (K, Double))(implicit dummy: DummyImplicit): Pmf[K] =
     Pmf(hist.updated(kv._1, hist.getOrElse(kv._1, 0.0) + kv._2))
+
   def mapValues(f: Double => Double)(implicit dummy: DummyImplicit): Pmf[K] = Pmf(hist.mapValues(f))
 
   /**
@@ -24,7 +25,7 @@ case class Pmf[K](hist: Map[K, Double]) extends Map[K, Double] with MapLike[K, D
    */
   def prob(key: K) = getOrElse(key, 0.0)
 
-  def prob(pred: K => Boolean) = toIterator.filter { case (k, _) => pred(k) }.map(_._2).sum
+  def prob(pred: K => Boolean) = iterator.filter { case (k, _) => pred(k) }.map(_._2).sum
 
   /**
    * Returns the highest probability with its associated value.
@@ -37,7 +38,7 @@ case class Pmf[K](hist: Map[K, Double]) extends Map[K, Double] with MapLike[K, D
    * @return the mean of the PMF.
    */
   def mean(implicit num: Numeric[K]): Double =
-    hist.map { case (h, prob) => num.toDouble(h) * prob }.sum
+    iterator.map { case (h, prob) => num.toDouble(h) * prob }.sum
 
   def random(): K = {
     def get(rand: Double, it: Iterator[(K, Double)]): K = {
@@ -55,10 +56,9 @@ case class Pmf[K](hist: Map[K, Double]) extends Map[K, Double] with MapLike[K, D
     mapValues { prob: Double => prob / sum }
   }
 
-  def mapKeys[K2](f: K => K2): Pmf[K2] =
-    foldLeft(Pmf.empty[K2]) { case (acc, (k, prob)) => acc + (f(k), prob) }.normalized
+  def mapKeys[K2](f: K => K2): Pmf[K2] = map { case (k, prob) => (f(k), prob) }.normalized
 
-  def join[J](other: Pmf[K], comb: (K, K) => J)(implicit num: Numeric[K]): Pmf[J] = {
+  def join[J](other: Pmf[K], comb: (K, K) => J): Pmf[J] = {
     foldLeft(Pmf.empty[J]) { case (acc, (k, prob)) =>
       other.foldLeft(acc) { case (acc2, (k2, prob2)) =>
         acc2 + (comb(k, k2), prob * prob2)
@@ -88,18 +88,18 @@ object Pmf {
     new mutable.Builder[(K, Double), Pmf[K]] {
       var hist = Map.empty[K, Double]
 
-      override def result() = Pmf(hist)
-      override def clear() { hist = Map.empty }
-      override def +=(elem: (K, Double)) = {
+      def result() = Pmf(hist)
+      def clear() { hist = Map.empty }
+      def +=(elem: (K, Double)) = {
         hist = hist.updated(elem._1, hist.getOrElse(elem._1, 0.0) + elem._2)
         this
       }
     }
 
-  implicit def canBuildFrom[K]: CanBuildFrom[Pmf[K], (K, Double), Pmf[K]] =
-    new CanBuildFrom[Pmf[K], (K, Double), Pmf[K]] {
-      override def apply() = builder[K]
-      override def apply(from: Pmf[K]) = builder[K]
+  implicit def canBuildFrom[K]: CanBuildFrom[Pmf[_], (K, Double), Pmf[K]] =
+    new CanBuildFrom[Pmf[_], (K, Double), Pmf[K]] {
+      def apply() = builder[K]
+      def apply(from: Pmf[_]) = builder[K]
     }
 
   def empty[K] = Pmf(Map.empty[K, Double])
