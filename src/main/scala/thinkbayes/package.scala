550b747749a37ba79483657719af771579f4e2f0
Rui Gonçalves <ruippeixotog@gmail.com>
2014-09-10 21:57:25 +0100

    Allow converting any histogram (e.g. a Map) to a Pmf

--- src/main/scala/thinkbayes/package.scala
+++ src/main/scala/thinkbayes/package.scala
@@ -6,6 +6,8 @@ package object thinkbayes {
     private def pad(str: String, n: Int): String =
       if(str.length > n) str.substring(0, n) else str + (" " * (n - str.length))
 
+    def toPmf = new Pmf(hist)
+
     def print()(implicit ord: Ordering[K]) {
       if(hist.nonEmpty) {
         val keyLen = hist.keys.map(_.toString.length).max
