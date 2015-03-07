7d940cfd7aab3002ef538b474f8a7ae5fa2b186d
Rui Gonçalves <ruippeixotog@gmail.com>
2015-03-07 00:19:05 +0000

    Make distribution Pmf factories share a common random number generator

--- src/main/scala/thinkbayes/extensions/Distributions.scala
+++ src/main/scala/thinkbayes/extensions/Distributions.scala
@@ -1,10 +1,12 @@
 package thinkbayes.extensions
 
 import org.apache.commons.math3.distribution._
+import org.apache.commons.math3.random.Well19937c
 import thinkbayes._
 import weka.estimators.KernelEstimator
 
 object Distributions {
+  private[this] val rndGen = new Well19937c()
 
   implicit def integerDistributionAsPmf(distrib: IntegerDistribution): Pmf[Int] = {
     val lower =
@@ -38,7 +40,7 @@ object Distributions {
     new Pdf { def density(x: Double) = kde.getProbability(x) }
   }
 
-  def normalPdf(mean: Double, stdev: Double): Pdf = new NormalDistribution(mean, stdev)
+  def normalPdf(mean: Double, stdev: Double): Pdf = new NormalDistribution(rndGen, mean, stdev)
 
   def normalPmf(mean: Double, stdev: Double, numSigmas: Double = 4.0, steps: Int = 2000): Pmf[Double] = {
     val low = mean - numSigmas * stdev
@@ -46,15 +48,18 @@ object Distributions {
     normalPdf(mean, stdev).toPmf(low to high by ((high - low) / steps))
   }
 
-  def poissonPmf(lam: Double): Pmf[Int] = if (lam > 0) new PoissonDistribution(lam) else Pmf.empty
+  def poissonPmf(lam: Double): Pmf[Int] =
+    if (lam <= 0) Pmf.empty
+    else new PoissonDistribution(rndGen, lam,
+      PoissonDistribution.DEFAULT_EPSILON, PoissonDistribution.DEFAULT_MAX_ITERATIONS)
 
-  def exponentialPdf(lam: Double): Pdf = new ExponentialDistribution(1.0 / lam)
+  def exponentialPdf(lam: Double): Pdf = new ExponentialDistribution(rndGen, 1.0 / lam)
 
   def exponentialPmf(lam: Double, high: Double = Double.PositiveInfinity, steps: Int = 2000): Pmf[Double] = {
-    val distrib = new ExponentialDistribution(1.0 / lam)
+    val distrib = new ExponentialDistribution(rndGen, 1.0 / lam)
     val realHigh = if (high.isPosInfinity) distrib.inverseCumulativeProbability(0.9999) else high
     distrib.toPmf(0.0 to realHigh by (realHigh / steps))
   }
 
-  def binomialPmf(trials: Int, p: Double): Pmf[Int] = new BinomialDistribution(trials, p)
+  def binomialPmf(trials: Int, p: Double): Pmf[Int] = new BinomialDistribution(rndGen, trials, p)
 }
