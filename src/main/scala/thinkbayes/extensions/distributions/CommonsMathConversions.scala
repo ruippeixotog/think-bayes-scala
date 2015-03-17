08305adb29fcbe56e4e05d5c7981552028bb13d9
Rui Gonçalves <ruippeixotog@gmail.com>
2015-03-17 00:59:26 +0000

    Convert commons-math map views into pmf views

--- src/main/scala/thinkbayes/extensions/distributions/CommonsMathConversions.scala
+++ src/main/scala/thinkbayes/extensions/distributions/CommonsMathConversions.scala
@@ -6,8 +6,7 @@ import CommonsMathConversions._
 
 trait CommonsMathConversions {
 
-  implicit def integerDistributionAsPmf(distrib: IntegerDistribution): Pmf[Int] =
-    Pmf(IntegerDistributionMapView(distrib))
+  implicit def integerDistributionAsPmf(distrib: IntegerDistribution): Pmf[Int] = IntegerDistributionPmf(distrib)
 
   implicit def realDistributionAsPdf(distrib: RealDistribution): Pdf = {
     if (!distrib.getSupportLowerBound.isNegInfinity && distrib.getSupportUpperBound.isPosInfinity) {
@@ -25,7 +24,7 @@ trait CommonsMathConversions {
 object CommonsMathConversions {
   val epsilon = 0.0001
 
-  case class IntegerDistributionMapView(distrib: IntegerDistribution) extends Map[Int, Double] {
+  case class IntegerDistributionPmf(distrib: IntegerDistribution) extends Pmf[Int] {
 
     private[this] lazy val lowerBound =
       if (distrib.getSupportLowerBound != Int.MinValue) distrib.getSupportLowerBound
@@ -34,23 +33,33 @@ object CommonsMathConversions {
       if (distrib.getSupportUpperBound != Int.MaxValue) distrib.getSupportUpperBound
       else distrib.inverseCumulativeProbability(1.0 - epsilon)
 
-    def +[B1 >: Double](kv: (Int, B1)) = iterator.toMap + kv
-    def -(key: Int) = iterator.toMap - key
+    def +[B1 >: Double](kv: (Int, B1)): Map[Int, B1] = toMap + kv
+    def -(key: Int): Pmf[Int] = toHistogramPmf - key
 
     def get(key: Int) = Some(distrib.probability(key))
     def iterator = (lowerBound to upperBound).iterator.map { key => (key, distrib.probability(key)) }
+
+    def +(kv: (Int, Double))(implicit dummy: DummyImplicit): Pmf[Int] = toHistogramPmf + kv
+    def mapValues(f: (Double) => Double)(implicit dummy: DummyImplicit): Pmf[Int] = toHistogramPmf.mapValues(f)
+
+    @inline private[this] def toHistogramPmf: Pmf[Int] = Pmf(iterator.toSeq: _*)
   }
 
-  case class RealDistributionMapView(distrib: RealDistribution, domain: Seq[Double]) extends Map[Double, Double] {
-    def +[B1 >: Double](kv: (Double, B1)) = iterator.toMap + kv
-    def -(key: Double) = iterator.toMap - key
+  case class RealDistributionPmf(distrib: RealDistribution, domain: Seq[Double]) extends Pmf[Double] {
+    def +[B1 >: Double](kv: (Double, B1)): Map[Double, B1] = toMap + kv
+    def -(key: Double): Pmf[Double] = toHistogramPmf - key
 
     def get(key: Double) = Some(distrib.probability(key))
     def iterator = domain.iterator.map { key => (key, distrib.density(key)) }
+
+    def +(kv: (Double, Double))(implicit dummy: DummyImplicit): Pmf[Double] = toHistogramPmf + kv
+    def mapValues(f: (Double) => Double)(implicit dummy: DummyImplicit): Pmf[Double] = toHistogramPmf.mapValues(f)
+
+    @inline private[this] def toHistogramPmf: Pmf[Double] = Pmf(iterator.toSeq: _*)
   }
 
-  object RealDistributionMapView {
-    def apply(distrib: RealDistribution, steps: Int): RealDistributionMapView = {
+  object RealDistributionPmf {
+    def apply(distrib: RealDistribution, steps: Int, stringRepr: String): RealDistributionPmf = {
       val lowerBound =
         if (!distrib.getSupportLowerBound.isNegInfinity) distrib.getSupportLowerBound
         else distrib.inverseCumulativeProbability(epsilon)
@@ -59,7 +68,7 @@ object CommonsMathConversions {
         if (!distrib.getSupportUpperBound.isPosInfinity) distrib.getSupportUpperBound
         else distrib.inverseCumulativeProbability(1.0 - epsilon)
 
-      this(distrib, lowerBound to upperBound by ((upperBound - lowerBound) / steps))
+      RealDistributionPmf(distrib, lowerBound to upperBound by ((upperBound - lowerBound) / steps))
     }
   }
 }
