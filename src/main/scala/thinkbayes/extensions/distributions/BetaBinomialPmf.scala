8107c643d1c741ee71a3a40282e565e0cb7ea120
Rui Gonçalves <ruippeixotog@gmail.com>
2015-04-07 12:34:19 +0100

    Add closed-form expression for the mode of a beta-binomial

--- src/main/scala/thinkbayes/extensions/distributions/BetaBinomialPmf.scala
+++ src/main/scala/thinkbayes/extensions/distributions/BetaBinomialPmf.scala
@@ -1,7 +1,7 @@
 package thinkbayes.extensions.distributions
 
 import org.apache.commons.math3.special.Gamma._
-import org.apache.commons.math3.util.FastMath
+import org.apache.commons.math3.util.FastMath._
 import thinkbayes.Pmf
 
 /**
@@ -10,17 +10,26 @@ import thinkbayes.Pmf
  * @param alpha the alpha parameter
  * @param beta the beta parameter
  */
-class BetaBinomialPmf(trials: Int, alpha: Double, beta: Double) extends Pmf[Int] with ClosedFormPmf[Int] {
+class BetaBinomialPmf(val trials: Int, val alpha: Double, val beta: Double) extends Pmf[Int] with ClosedFormPmf[Int] {
 
   private[this] def logP(k: Int) =
     (logGamma(k + alpha) + logGamma(trials - k + beta) + logGamma(alpha + beta) + logGamma(trials + 2)) -
-      (FastMath.log(trials + 1) + logGamma(alpha + beta + trials) +
+      (log(trials + 1) + logGamma(alpha + beta + trials) +
         logGamma(alpha) + logGamma(beta) + logGamma(k + 1) + logGamma(trials - k + 1))
 
-  @inline private[this] def p(k: Int) = FastMath.exp(logP(k))
+  @inline private[this] def p(k: Int) = exp(logP(k))
 
   def get(key: Int) = if (key < 0 || key > trials) None else Some(p(key))
   def iterator = Iterator.tabulate(trials + 1) { key => (key, p(key)) }
+
+  override def maxProb = { val m = mode; (m, prob(m)) }
+
+  override def mode =
+    if (alpha > 1.0 && beta > 1.0) round(trials * (alpha - 1.0) / (alpha + beta - 2.0)).toInt
+    else if (alpha == 1.0 && beta == 1.0) 0 // or any other value in the range [0, `trials`]
+    else if (alpha < 1.0 && beta < 1.0) 0 // or `trials`
+    else if (alpha < 1.0 && beta >= 1.0 || alpha == 1.0 && beta > 1.0) 0
+    else trials
 
   override def mean(implicit num: Numeric[Int]) = trials * alpha / (alpha + beta)
 
