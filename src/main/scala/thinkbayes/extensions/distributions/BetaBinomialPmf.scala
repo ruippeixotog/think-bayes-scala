fb946c5ad37f9b8e161b822047ffb030ac2c4af1
Rui Gonçalves <ruippeixotog@gmail.com>
2015-04-13 00:47:55 +0100

    Simplify beta-binomial pmf expression

--- src/main/scala/thinkbayes/extensions/distributions/BetaBinomialPmf.scala
+++ src/main/scala/thinkbayes/extensions/distributions/BetaBinomialPmf.scala
@@ -1,6 +1,7 @@
 package thinkbayes.extensions.distributions
 
-import org.apache.commons.math3.special.Gamma._
+import org.apache.commons.math3.special.Beta._
+import org.apache.commons.math3.util.CombinatoricsUtils._
 import org.apache.commons.math3.util.FastMath._
 import thinkbayes.Pmf
 
@@ -13,9 +14,7 @@ import thinkbayes.Pmf
 class BetaBinomialPmf(val trials: Int, val alpha: Double, val beta: Double) extends Pmf[Int] with ClosedFormPmf[Int] {
 
   private[this] def logP(k: Int) =
-    (logGamma(k + alpha) + logGamma(trials - k + beta) + logGamma(alpha + beta) + logGamma(trials + 2)) -
-      (log(trials + 1) + logGamma(alpha + beta + trials) +
-        logGamma(alpha) + logGamma(beta) + logGamma(k + 1) + logGamma(trials - k + 1))
+    binomialCoefficientLog(trials, k) + logBeta(k + alpha, trials - k + beta) - logBeta(alpha, beta)
 
   @inline private[this] def p(k: Int) = exp(logP(k))
 
