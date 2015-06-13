00fc5ce5daba4901ef94b7e6e7219ed6fa9f00bd
Rui Gonçalves <ruippeixotog@gmail.com>
2015-06-13 02:44:53 +0100

    Move jfreechart themes to a separate file

--- src/main/scala/thinkbayes/extensions/Plotting.scala
+++ src/main/scala/thinkbayes/extensions/Plotting.scala
@@ -1,24 +1,18 @@
 package thinkbayes.extensions
 
-import java.awt._
-import javax.swing.UIManager
 import org.jfree.chart._
-import org.jfree.chart.block.LineBorder
-import org.jfree.chart.title.{ LegendTitle, Title }
-import org.jfree.chart.plot.DefaultDrawingSupplier
-import org.jfree.chart.renderer.category.{ BarRenderer, CategoryItemRenderer, StandardBarPainter }
-import org.jfree.chart.renderer.xy.StandardXYBarPainter
 import org.jfree.data.category.DefaultCategoryDataset
 import org.jfree.data.xy.XYSeriesCollection
-import org.jfree.ui.RectangleInsets
+import thinkbayes._
+import thinkbayes.extensions.plotting.ThinkBayesChartTheme
+
 import scala.swing.Swing._
 import scala.util.Try
 import scalax.chart._
 import scalax.chart.api._
-import thinkbayes._
 
 trait Plotting {
-  val defaultTheme = darkChartTheme
+  val defaultTheme = ThinkBayesChartTheme.Dark
   val drawFrame = true
 
   implicit def mapAsPlottable[K, V](map: Map[K, V])(implicit asNum: Numeric[V]) = new Plottable[K] {
@@ -203,101 +197,6 @@ trait Plotting {
       panel.setMaximumDrawHeight(Int.MaxValue)
       frame.size = dim
       frame.visible = true
-    }
-  }
-
-  object ThinkBayesChartTheme {
-
-    final val LightSeriesColors = Array[Paint](
-      Color.green.darker(),
-      Color.red.darker(),
-      Color.blue.darker())
-
-    final val LightSeriesStrokes = Array[Stroke](
-      new BasicStroke(2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND))
-
-    final val DarkSeriesColors = Array[Paint](
-      new Color(160, 255, 160, 128),
-      new Color(255, 160, 160, 128),
-      new Color(160, 160, 255, 128))
-
-    final val DarkSeriesStrokes = LightSeriesStrokes
-  }
-
-  class ThinkBayesChartTheme(name: String) extends StandardChartTheme(name, false) {
-    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
-
-    setExtraLargeFont(new Font("Helvetica Neue", Font.BOLD, 20))
-    setLargeFont(new Font("Helvetica Neue", Font.BOLD, 14))
-    setRegularFont(new Font("Helvetica Neue", Font.PLAIN, 12))
-    setSmallFont(new Font("Helvetica Neue", Font.PLAIN, 10))
-
-    setAxisOffset(RectangleInsets.ZERO_INSETS)
-
-    setPlotOutlinePaint(new Color(0, 0, 0, 0))
-
-    setBarPainter(new StandardBarPainter())
-    setXYBarPainter(new StandardXYBarPainter())
-
-    override def applyToCategoryItemRenderer(renderer: CategoryItemRenderer) {
-      super.applyToCategoryItemRenderer(renderer)
-      renderer match {
-        case br: BarRenderer => br.setItemMargin(0.0)
-      }
-    }
-  }
-
-  lazy val lightChartTheme: ChartTheme = new ThinkBayesChartTheme("think-bayes-light") {
-
-    setChartBackgroundPaint(UIManager.getColor("Panel.background"))
-    setPlotBackgroundPaint(Color.white)
-
-    setDomainGridlinePaint(Color.lightGray)
-    setRangeGridlinePaint(Color.lightGray)
-
-    setDrawingSupplier(new DefaultDrawingSupplier(
-      ThinkBayesChartTheme.LightSeriesColors,
-      DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
-      DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
-      ThinkBayesChartTheme.LightSeriesStrokes,
-      DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
-      DefaultDrawingSupplier.DEFAULT_SHAPE_SEQUENCE))
-  }
-
-  lazy val darkChartTheme: ChartTheme = new ThinkBayesChartTheme("think-bayes-dark") {
-
-    setTitlePaint(Color.white)
-    setSubtitlePaint(Color.white)
-
-    setChartBackgroundPaint(new Color(31, 32, 27))
-    setPlotBackgroundPaint(new Color(31, 32, 27))
-
-    setDomainGridlinePaint(Color.white)
-    setRangeGridlinePaint(Color.white)
-
-    setLegendBackgroundPaint(new Color(31, 32, 27))
-    setLegendItemPaint(Color.white)
-    setLabelLinkPaint(new Color(0, 0, 0, 0))
-
-    setAxisLabelPaint(Color.white)
-    setTickLabelPaint(Color.white)
-
-    setDrawingSupplier(new DefaultDrawingSupplier(
-      ThinkBayesChartTheme.DarkSeriesColors,
-      DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
-      DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
-      ThinkBayesChartTheme.DarkSeriesStrokes,
-      DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
-      DefaultDrawingSupplier.DEFAULT_SHAPE_SEQUENCE))
-
-    override def applyToTitle(title: Title) {
-      super.applyToTitle(title)
-      title match {
-        case lt: LegendTitle =>
-          lt.setFrame(new LineBorder(new Color(0, 0, 0, 0),
-            new BasicStroke(),
-            RectangleInsets.ZERO_INSETS))
-      }
     }
   }
 }
---
+++ src/main/scala/thinkbayes/extensions/plotting/ThinkBayesChartTheme.scala
@@ -0,0 +1,105 @@
+package thinkbayes.extensions.plotting
+
+import java.awt.{ Stroke, BasicStroke, Color, Font }
+import javax.swing.UIManager
+
+import org.jfree.chart.StandardChartTheme
+import org.jfree.chart.block.LineBorder
+import org.jfree.chart.plot.DefaultDrawingSupplier
+import org.jfree.chart.renderer.category.{ BarRenderer, CategoryItemRenderer, StandardBarPainter }
+import org.jfree.chart.renderer.xy.StandardXYBarPainter
+import org.jfree.chart.title.{ LegendTitle, Title }
+import org.jfree.ui.RectangleInsets
+
+import scalax.chart.api._
+
+class ThinkBayesChartTheme(name: String) extends StandardChartTheme(name, false) {
+
+  def strokeSequence: Array[Stroke] = Array(
+    new BasicStroke(2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND))
+
+  def paintSequence: Array[Paint] =
+    DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE
+
+  UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
+
+  setExtraLargeFont(new Font("Helvetica Neue", Font.BOLD, 20))
+  setLargeFont(new Font("Helvetica Neue", Font.BOLD, 14))
+  setRegularFont(new Font("Helvetica Neue", Font.PLAIN, 12))
+  setSmallFont(new Font("Helvetica Neue", Font.PLAIN, 10))
+
+  setAxisOffset(RectangleInsets.ZERO_INSETS)
+
+  setPlotOutlinePaint(ThinkBayesChartTheme.Transparent)
+
+  setBarPainter(new StandardBarPainter())
+  setXYBarPainter(new StandardXYBarPainter())
+
+  setLegendBackgroundPaint(ThinkBayesChartTheme.Transparent)
+  setLabelLinkPaint(ThinkBayesChartTheme.Transparent)
+
+  setDrawingSupplier(new DefaultDrawingSupplier(
+    paintSequence,
+    DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
+    DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
+    strokeSequence,
+    DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
+    DefaultDrawingSupplier.DEFAULT_SHAPE_SEQUENCE))
+
+  override def applyToCategoryItemRenderer(renderer: CategoryItemRenderer) {
+    super.applyToCategoryItemRenderer(renderer)
+    renderer match {
+      case br: BarRenderer => br.setItemMargin(0.0)
+    }
+  }
+
+  override def applyToTitle(title: Title) {
+    super.applyToTitle(title)
+    title match {
+      case lt: LegendTitle =>
+        lt.setFrame(new LineBorder(new Color(0, 0, 0, 0),
+          new BasicStroke(),
+          RectangleInsets.ZERO_INSETS))
+    }
+  }
+}
+
+object ThinkBayesChartTheme {
+  final val Transparent = new Color(0, 0, 0, 0)
+
+  object Light extends ThinkBayesChartTheme("think-bayes-light") {
+
+    override def paintSequence = Array(
+      new Color(236, 93, 87),
+      new Color(112, 191, 65),
+      new Color(81, 167, 249))
+
+    setChartBackgroundPaint(Color.white)
+    setPlotBackgroundPaint(Color.white)
+
+    setDomainGridlinePaint(Color.lightGray)
+    setRangeGridlinePaint(Color.lightGray)
+  }
+
+  object Dark extends ThinkBayesChartTheme("think-bayes-dark") {
+
+    override def paintSequence = Array(
+      new Color(160, 255, 160, 128),
+      new Color(255, 160, 160, 128),
+      new Color(160, 160, 255, 128))
+
+    setTitlePaint(Color.white)
+    setSubtitlePaint(Color.white)
+
+    setChartBackgroundPaint(new Color(31, 32, 27))
+    setPlotBackgroundPaint(new Color(31, 32, 27))
+
+    setDomainGridlinePaint(Color.white)
+    setRangeGridlinePaint(Color.white)
+
+    setLegendItemPaint(Color.white)
+
+    setAxisLabelPaint(Color.white)
+    setTickLabelPaint(Color.white)
+  }
+}
